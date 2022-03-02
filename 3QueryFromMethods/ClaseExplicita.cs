03eb58bf8d5ccdc88af52c41be07229ac3987a8d
David Amador <54488581+dsamador@users.noreply.github.com>
2022-03-02 12:01:57 -0500

    query from methods

---
+++ 3QueryFromMethods/ClaseExplicita.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3QueryFromMethods
+{
+    internal class ClaseExplicita
+    {
+        private static string[] postres = { "pay de manzana", "tres leches", "fresas con crema", "fresas con almibar" };
+
+        //creamos el query
+        //no puede usarse de forma implicita
+        //debe ser estatico
+        private static IEnumerable<string> encontrados = from p in postres 
+                                                         where p.Contains("fresas") 
+                                                         orderby p select p;
+        private static int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+        //el metodo regresa y todo se trabaja internamente
+        public static IEnumerable<int> ObtenerNumerosPares()
+        {
+            
+            var pares = numeros.Where(x => x % 2 == 0);
+            return pares;
+        }
+
+        public static IEnumerable<string> ObtenerPostres()
+        {
+            return encontrados;
+        }
+
+        //metodo que regresa el resultado de un query inmediato
+        public static IEnumerable<int> Impares()
+        {
+            var impares = numeros.Where(x => x % 2 != 0);
+            return impares.ToArray();
+        }
+    }
+}
---
+++ 3QueryFromMethods/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3QueryFromMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //obtener resultados de query desde metodos
+
+            //invocamos el metodo
+            IEnumerable<int> resultados = ClaseExplicita.ObtenerNumerosPares();
+
+            //mostramos los resultados
+            foreach (var item in resultados)
+                Console.WriteLine(item);
+
+            Console.WriteLine("----------");
+
+            IEnumerable<string> postres = ClaseExplicita.ObtenerPostres();
+
+            //resultados
+            foreach (var item in postres)
+                Console.WriteLine(item);
+
+            Console.WriteLine("----------");
+
+            var impares = ClaseExplicita.Impares();
+            foreach (var item in impares)
+                Console.WriteLine(item);
+        }        
+    }
+}
