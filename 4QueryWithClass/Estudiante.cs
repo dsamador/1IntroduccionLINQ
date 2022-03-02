506ecf9ac472c6086c1ad2e3f0c5ddb286f836a7
David Amador <54488581+dsamador@users.noreply.github.com>
2022-03-02 14:57:13 -0500

    Queries to a class

---
+++ 4QueryWithClass/Estudiante.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4QueryWithClass
+{
+    internal class Estudiante
+    {
+        public string Nombre { get; }
+        public string Id { get; }
+        public string Curso { get; }
+        public double Promedio { get; }
+
+        public Estudiante(
+            string nombre, 
+            string id, 
+            string curso, 
+            double promedio)
+        {
+            Nombre = nombre;
+            Id = id;
+            Curso = curso;
+            Promedio = promedio;
+        }
+        public override string ToString()
+        {
+            return $"Nombre {Nombre}, Id {Id}, " +
+                   $"Curso {Curso}, Promedio {Promedio}";
+        }
+    }
+}
---
+++ 4QueryWithClass/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4QueryWithClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //LINQ con Clases
+            //Creamos una lista
+            List<Estudiante> estudiantes = new List<Estudiante>
+            {
+                new Estudiante("Ana", "A100", "Mercadotecnia", 10.0),
+                new Estudiante("Rodrigo", "S200", "POO", 9.0),
+                new Estudiante("Angelie", "A110", "Mercadotecnia", 4.0),
+                new Estudiante("Andres", "A130", "Mercadotecnia", 8.7),
+                new Estudiante("Luisa", "S233", "Fundamentos", 4.3),
+                new Estudiante("Silena", "S100", "POO", 8.3)
+            };
+
+            //encontramos los reprobados
+            var reprobados = from e in estudiantes
+                             where e.Promedio <= 5.0
+                             select e;
+            //mostramos
+            Console.WriteLine("Reprobados");
+            foreach (Estudiante r in reprobados)            
+                Console.WriteLine(r);
+
+            //mostramos solo un atributo
+            Console.WriteLine("Solo nombre de reprobados");
+            foreach (Estudiante r in reprobados)
+                Console.WriteLine(r.Nombre);
+
+            //Encontramos solo los nombre de los de mercadotecnia
+            Console.WriteLine("Los de mercadotecnia");
+            var mercadotecnia = from e in estudiantes
+                                where e.Curso.Contains("Mercadotecnia")
+                                //where e.Curso == "Mercadotecnia"
+                                select e.Nombre;
+            
+            foreach (string r in mercadotecnia)
+                Console.WriteLine(r);
+
+            
+        }
+    }
+}
